4f60e86d83eb20a2fe7e85260500a570f818231a
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-06-05 19:25:11 +0300

    initial commit

---
+++ e2e/popover.test.js
@@ -0,0 +1,47 @@
+import puppeteer from "puppeteer";
+
+describe('test popover', () => {
+    let browser;
+    let page;
+
+    beforeEach(async () => {
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 100,
+            devtools: true,
+        });
+
+        page = await browser.newPage();
+    })
+
+    test('open popup of popover', async () => {
+        await page.goto('http://localhost:9000');
+
+        await page.waitFor('.container');
+
+        const button = await page.$('.popover-button');
+
+        await button.click();
+
+        await page.screenshot({path: 'screenshot-open.png'})
+
+        await page.waitFor('.container .popover__wr-popup.active');
+    })
+
+    test('close popup of popover', async () => {
+        await page.goto('http://localhost:9000');
+
+        await page.waitFor('.container');
+
+        const button = await page.$('.popover-button');
+
+        await button.click();
+        await button.click();
+
+        await page.screenshot({path: 'screenshot-close.png'})
+    })
+
+    afterEach(async () => {
+        await browser.close()
+    })
+})
---
+++ src/app/app.js
@@ -0,0 +1,6 @@
+import Popover2 from "./popover/popover";
+
+const container = document.querySelector('.container');
+
+const popover = new Popover2(container);
+popover.bindToDom();
