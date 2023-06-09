8c5ef96758c948ab0b647ee0fcda99d2d401fe63
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-06-09 14:45:00 +0300

    changed config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
+module.exports = { 
+  mode: 'development',
   devServer: {
     port: 9000,
   },
